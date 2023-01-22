db0ca9ac419be63a3ba7a06e5d6dc5e801438b39
AboveBison <gagestroud1995@gmail.com>
2023-01-22 17:53:58 -0500

    Completed the reverse string exercise!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+    let result = "";
+    for (let i = stringToReverse.length; i >= 0; --i) {
+        result += stringToReverse.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
