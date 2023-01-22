322652d76bf58ae2027c673c5108104eaf154c25
AboveBison <gagestroud1995@gmail.com>
2023-01-22 17:47:06 -0500

    Completed the stringRepeat exercise!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numOfRepeats) {
+    if (numOfRepeats < 0) {
+        return "ERROR";
+    }
 
+    let result = "";
+    for (let i = 0; i < numOfRepeats; ++i) {
+        result += stringToRepeat;
+    }
+    return result;
 };
 
 // Do not edit below this line
